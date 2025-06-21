707ce68a3b8c94094fd246dfb725abb78c656206
Ryan Kubik <rmkubik@me.com>
2025-06-21 12:02:58 -0700

    Rough mouth eating animation

--- goal.gd
+++ goal.gd
@@ -4,20 +4,22 @@ class_name Goal
 var area: Area2D
 var collisionShape: CollisionShape2D
 var isColliding: bool
+@onready var animatedSprite: AnimatedSprite2D = $Area2D/AnimatedSprite2D
 
 func _ready() -> void:
 	area = get_node("Area2D")
 	collisionShape = get_node('Area2D/CollisionShape2D')
+	animatedSprite.play("open")
 	
 
-func _draw():
-	#var collisionShape: CollisionShape2D = get_node('Area2D/CollisionShape2D')
-	#print(collisionShape.shape.get_rect().size.x)
-	#print($".".position)
-	var color = Color.hex(0xa9ff9cff)
-	if (isColliding):
-		color = Color.hex(0x13ad3cff)
-	draw_circle(Vector2(0,0), collisionShape.shape.radius, color)
+#func _draw():
+	##var collisionShape: CollisionShape2D = get_node('Area2D/CollisionShape2D')
+	##print(collisionShape.shape.get_rect().size.x)
+	##print($".".position)
+	#var color = Color.hex(0xa9ff9cff)
+	#if (isColliding):
+		#color = Color.hex(0x13ad3cff)
+	#draw_circle(Vector2(0,0), collisionShape.shape.radius, color)
 
 	
 #func _process(delta):
@@ -28,6 +30,9 @@ func _draw():
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print('entered')
+	if body.get_parent() is Target:
+		animatedSprite.play("lick")
+		body.get_parent().queue_free()
 	isColliding = true
 	queue_redraw()
 
@@ -35,3 +40,9 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 	print('exited')
 	isColliding = false
 	queue_redraw()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animatedSprite.animation == 'lick':
+		animatedSprite.play('closed')
+	
---
+++ target.gd
@@ -0,0 +1,2 @@
+class_name Target
+extends Node2D
